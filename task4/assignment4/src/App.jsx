da81169a1ef61a9a6cd94e7adfd658d0a202a3a1
hanivaghani <vaghanihani@gmail.com>
2025-02-12 22:11:31 +0530

    file completed

---
+++ task4/assignment4/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import React from "react";
+import WelcomeMessage from "./components/welcomeMessage";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
