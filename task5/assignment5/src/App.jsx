0b82a5c997d8934724105549cd5a176ae1c321f2
hanivaghani <vaghanihani@gmail.com>
2025-02-12 22:27:44 +0530

    files changes

---
+++ task5/assignment5/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import React from "react";
+import JSXRules from "./components/JSXRules";
+
+function App() {
+  return (
+    <div>
+      <JSXRules />
+    </div>
+  );
+}
+
+export default App;
+
