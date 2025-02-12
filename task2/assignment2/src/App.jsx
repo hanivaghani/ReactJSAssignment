08e5a9d835e2079419c22537909b077bb1afde34
hanivaghani <vaghanihani@gmail.com>
2025-02-12 16:18:25 +0530

    task2 completed

---
+++ task2/assignment2/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import React from 'react'
+import Greetings from "./components/Greet"
+
+const App = () => {
+  return (
+    <>
+      <Greetings/>
+    </>
+  )
+}
+
+export default App;
